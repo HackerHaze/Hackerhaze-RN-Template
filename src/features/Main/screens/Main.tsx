1cd72c25097d0eaf52dce8aa987974b8ec32af50
Matt <matt@hackerhaze.com>
2023-10-26 18:01:11 +0100

    feat: :fire: Add React-Native-Reanimated

--- src/features/Main/screens/Main.tsx
+++ src/features/Main/screens/Main.tsx
@@ -2,13 +2,40 @@ import {NavigationFunctionComponent} from 'react-native-navigation';
 import {ScrollView} from 'react-native';
 import React from 'react';
 import Logo from '@assets/images/logo.svg';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withRepeat,
+  withTiming,
+} from 'react-native-reanimated';
 // import {localization} from '../../common/localization/localization';
 // import {CommonStyles} from '../../core/theme/commonStyles';
 
 const MainScreen: NavigationFunctionComponent = () => {
+  const translateY = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{translateY: translateY.value}],
+    };
+  });
+
+  React.useEffect(() => {
+    translateY.value = withRepeat(withTiming(-20, {duration: 1000}), -1, true);
+  }, [translateY]);
   return (
-    <ScrollView contentInsetAdjustmentBehavior={'automatic'} style={{}}>
-      <Logo width={50} height={50} />
+    <ScrollView contentInsetAdjustmentBehavior={'automatic'}>
+      <Animated.View
+        style={[
+          {
+            flex: 1,
+            paddingTop: 20,
+            alignItems: 'center',
+          },
+          animatedStyle,
+        ]}>
+        <Logo width={50} height={50} />
+      </Animated.View>
     </ScrollView>
   );
 };
