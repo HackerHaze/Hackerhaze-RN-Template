1c3e7863a05ec14e1f0985f9c83564be048e6384
Matt <matt@hackerhaze.com>
2023-10-30 12:28:47 +0000

    feat: :zap: Add Localisation/translations

---
+++ src/shared/localization/translations/emptyLocalization.ts
@@ -0,0 +1,10 @@
+export const emptyLocalization = {
+  en: {
+    noData: 'No data',
+    checkThisPageLater: 'Check this page later',
+  },
+  es: {
+    noData: 'No hay datos',
+    checkThisPageLater: 'Revisa esta página más tarde',
+  },
+};
