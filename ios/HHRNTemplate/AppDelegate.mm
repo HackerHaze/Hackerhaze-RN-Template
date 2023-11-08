b5fccaeb04b611987eacaa1e34cd971cada22bca
Matt <matt@hackerhaze.com>
2023-11-08 11:23:58 +0000

    feat: Finish AppCenter + Firebase Integration for iOS

--- ios/HHRNTemplate/AppDelegate.mm
+++ ios/HHRNTemplate/AppDelegate.mm
@@ -2,17 +2,35 @@
 #import <Firebase.h>
 #import <ReactNativeNavigation/ReactNativeNavigation.h>
 
+#import <AppCenterReactNativeShared/AppCenterReactNativeShared.h>
+#import <AppCenterReactNative.h>
+
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  NSString *filePath = [[NSBundle mainBundle] pathForResource:@"GoogleService-Info" ofType:@"plist"];
+  NSString *firebaseFilePath = [[NSBundle mainBundle] pathForResource:@"GoogleService-Info" ofType:@"plist"];
+  NSDictionary *firebaseConfig = [NSDictionary dictionaryWithContentsOfFile:firebaseFilePath];
+  NSString *clientID = firebaseConfig[@"CLIENT_ID"];
 
-  if ([[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
+  if (![clientID isEqualToString:@"{Your Client ID}"]) {
     [FIRApp configure];
   }
+
+  NSString *appCenterFilePath = [[NSBundle mainBundle] pathForResource:@"AppCenterConfig" ofType:@"plist"];
+  NSDictionary *appCenterConfig = [NSDictionary dictionaryWithContentsOfFile:appCenterFilePath];
+  NSString *appSecret = appCenterConfig[@"AppSecret"];
+
+  if (![appSecret isEqualToString:@"{Your App Secret}"]) {
+    [AppCenterReactNativeShared setStartAutomatically:YES];
+    [AppCenterReactNativeShared setAppSecret:appSecret];
+    [AppCenterReactNative register];
+    // [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+    // [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
+  }
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
