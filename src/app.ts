756cc68a6956e545445c0322e2d188b46c1f8305
Matt <matt@hackerhaze.com>
2023-11-09 14:06:20 +0000

    feat: :fire: After spike on how to better implement this and failed attempts, we will go with this implementation over the client side

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,16 @@ import {
   registerComponents,
   setDefaultOptions,
 } from '@shared/navigation/navigation';
+import CodePush, {SyncOptions} from 'react-native-code-push';
+
+const codePushOptions: SyncOptions = {
+  installMode: CodePush.InstallMode.IMMEDIATE,
+};
 
 export function initializeApp() {
   //   setLanguage();
   //   setDefaultOrientation();
+  CodePush.sync(codePushOptions);
   registerComponents();
   setDefaultOptions();
   Navigation.events().registerAppLaunchedListener(async () => {
