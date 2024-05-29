e846745e2673536303ae2f8317f751e61a47fab8
Matheus <matheus_paranhos@outlook.com.br>
2024-05-29 20:00:41 +0100

    feat: Add minimal implementation of tanstack query

--- template/App.tsx
+++ template/App.tsx
@@ -7,36 +7,39 @@ import AuthScreen from '@features/Auth/screens';
 import MainScreen from '@features/Main/screens';
 import {Provider} from 'react-redux';
 import {store} from '@redux/store';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 const Stack = createNativeStackNavigator();
-
+const queryClient = new QueryClient();
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Splash">
-          <Stack.Screen
-            name="Splash"
-            component={SplashScreen}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="OnboardingStack"
-            component={OnBoarding}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="AuthStack"
-            component={AuthScreen}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="MainStack"
-            component={MainScreen}
-            options={{headerShown: false}}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <QueryClientProvider client={queryClient}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Splash">
+            <Stack.Screen
+              name="Splash"
+              component={SplashScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="OnboardingStack"
+              component={OnBoarding}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="AuthStack"
+              component={AuthScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="MainStack"
+              component={MainScreen}
+              options={{headerShown: false}}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </QueryClientProvider>
     </Provider>
   );
 };
